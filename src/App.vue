37b1b4ea95389b4ae60506797c2e36b153d95dc1
Marco Fonti <theemporriumg@gmail.com>
2024-06-28 15:40:04 +0200

    add composition api method ref

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<!-- JS -->
+<script>
+/* IMPORTAZIONE DEL REF */
+import { ref } from 'vue';
+
+export default {
+    name: 'NomeFile',
+
+    /* LOGICA DEL COMPONENTE */
+    setup() {
+
+        /* VARIABILI REATTIVI GRAZIE AL REF */
+        const message = ref('hello')
+        const quantity = ref(1)
+
+        /* INCREMENTIAMO E DECREMENTIAMO , '.VALUE' = PER VEDERE IL VALORE CORRENTE DI QUANTITY */
+        const increment = () => quantity.value++
+        const decrement = () => quantity.value--
+
+        /* RESTITUISCO PER VEDERE IN PAGINA */
+        return {
+            message,
+            quantity,
+            increment,
+            decrement
+        }
+    }
+};
+
 </script>
 
+<!-- HTML -->
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+    <!-- MESSAGGIO -->
+    <h1>{{ message }}</h1>
+
+    <!-- NUMERO DI QUANTITA' -->
+    <h2>{{ quantity }}</h2>
+
+    <!-- INCREMENTO -->
+    <button @click="increment"> + </button>
+
+    <!-- DECREMENTO -->
+    <button @click="decrement"> - </button>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<!-- CSS -->
+<style scoped></style>
